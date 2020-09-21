85d349006dbe8847961d3f7d49a2a4430e0782e9
Rombertik-root <71608086+Rombertik-root@users.noreply.github.com>
2020-09-20 22:15:55 -0400

    Create mac_spoofer.py

---
+++ mac_spoofer.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+import re
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address of")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New Mac Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error(" Please specify an interface name, use --help for help")
+    elif not options.new_mac:
+        parser.error(" Please specify a new mac address, use --help for help")
+    return options
+
+
+def mac_spoofer(interface, new_mac):
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+def mac_check(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[+] Could not read MAC address.")
+
+options = get_arguments()
+
+current_mac = mac_check(options.interface)
+print("[+] Current MAC address = " + str(current_mac))
+
+mac_spoofer(options.interface, options.new_mac)
+print("[+] Changing MAC address of " + options.interface + " to " + options.new_mac)
+
+current_mac = mac_check(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address of " + options.interface + " was changed to " + current_mac)
+else:
+    print("[+] MAC address was not changed.")
